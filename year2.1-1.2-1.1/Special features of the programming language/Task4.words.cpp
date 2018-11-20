19ec4510a916017d88cc0e7e90aefc5e16392613
5UK30M1 <44413697+5UK30M1@users.noreply.github.com>
2018-11-20 15:48:09 +0200

    Create Task4.words.cpp

---
+++ year2.1-1.2-1.1/Special features of the programming language/Task4.words.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+
+    cin>>n;
+
+    int **arr = new int* [n];
+
+    for(int i = 0; i < n; i++){
+        int *arr = new int [n];
+    }
+
+}
