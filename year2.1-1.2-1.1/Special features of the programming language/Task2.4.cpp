0c817992974dfa557bb166173e723365c6cafe75
5UK30M1 <44413697+5UK30M1@users.noreply.github.com>
2018-11-20 15:43:14 +0200

    Update Task2.4.cpp

--- year2.1-1.2-1.1/Special features of the programming language/Task2.4.cpp
+++ year2.1-1.2-1.1/Special features of the programming language/Task2.4.cpp
@@ -4,18 +4,30 @@ using namespace std;
 
 int main()
 {
-    double num;
-    char ch;
-    cout << "Enter real number" << endl;
-    cin >> num;
-    num *= num;
-    cout << num << endl;
-    cout << "Enter 'y' to continue" << endl;
-    cin >> ch;
-    if (ch == 'y') {
-        main();
-    }
-    else {
+    int n,old,young,i;
+    float avg = 0;
+    cout << "Enter age of customer (Enter 0 to finish):" << endl;
+    cin >> n;
+    old = n;
+    young = n;
+    avg = n;
+    if (n == 0)
+    {
         return 0;
     }
+    for (i = 0; n != 0; i++)
+    {
+        cout << "Enter age of customer (Enter 0 to finish):" << endl;
+        cin >> n;
+        if (n > old && n != 0 ) {
+                old = n;
+        }
+        if (n < young && n != 0) {
+                young = n;
+        }
+        avg += n;
+    }
+    avg = avg / i;
+    cout << "Age of the oldest customer: " << old << endl << "Age of the youngest: " << young << endl << "Average age of customers: " << avg;
+    return 0;
 }
